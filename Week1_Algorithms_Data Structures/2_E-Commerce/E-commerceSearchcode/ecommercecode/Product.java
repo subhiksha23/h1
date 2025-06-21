f7cf97dfdcaf729dc274769a96305a11a291f3f2
subhiksha siva <161231594+subhiksha23@users.noreply.github.com>
2025-06-21 19:46:23 +0530

    Add files via upload

---
+++ Week1_Algorithms_Data Structures/2_E-Commerce/E-commerceSearchcode/ecommercecode/Product.java
@@ -0,0 +1,11 @@
+package ecommercecode;
+class Product {
+    int id;
+    String name;
+    String category;
+    Product(int id, String name, String category) {
+        this.id = id;
+        this.name = name;
+        this.category = category;
+    }
+}
